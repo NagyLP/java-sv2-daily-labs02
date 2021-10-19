e3bd806a7c375615b729f06359ba89fb7170cfb6
NagyLP <nagy.peter@pr.hu>
2021-10-19 15:41:22 +0200

    KONZ-F: Rectangle (osztály)

---
+++ src/main/java/day02/Rectangle.java
@@ -0,0 +1,20 @@
+package day02;
+
+public class Rectangle {
+
+    private double aSide;
+    private double bSide;
+
+    public Human (double aSide, double bSide) {
+        this.aSide = aSide;
+        this.bSide = bSide;
+    }
+
+    public double getaSide() {return aSide;}
+    public double getbSide() {return bSide;}
+
+    public void setaSide(double distance) {this.aSide = distance;}
+    public void setbSide(double distance) {this.bSide = distance;}
+
+    public String getInfo() {return "Egyik oldal: " +aSide+ " ; " +" Másik oldal: " +bSide;}
+}
